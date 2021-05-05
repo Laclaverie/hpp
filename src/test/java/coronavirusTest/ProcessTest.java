3df6832be635a877f030c167702244c6f6c248b2
laclaverie.pierre <pierre.laclaverie@telecom-st-etienne.fr>
2021-05-05 13:50:40 +0200

    res

--- src/test/java/coronavirusTest/ProcessTest.java
+++ src/test/java/coronavirusTest/ProcessTest.java
@@ -11,11 +11,12 @@ public class ProcessTest {
 
 	@Test
 	public void test() {
-		 ArrayBlockingQueue<String> readerqueue_; // non utilisé dans un premier temps
-		 ArrayBlockingQueue<String> writterqueue_; // non utilisé dans un premier temps
+		 ArrayBlockingQueue<String> readerqueue_=new ArrayBlockingQueue<String>(20); // non utilisé dans un premier temps
+		 ArrayBlockingQueue<String> writterqueue_=new ArrayBlockingQueue<String>(20); // non utilisé dans un premier temps
 
 		Process monP =new Process(readerqueue_,writterqueue_);
 		String s1 = "10,1552244,2";
+		String r1[] =monP.getInformation(s1);
 	 
 	}
 
