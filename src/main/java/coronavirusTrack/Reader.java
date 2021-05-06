fe9c6ee218f26a713a7ae91bfd8a10d8d869a4b4
julienchavas <julien.chavas1999@gmail.com>
2021-05-06 11:52:06 +0200

    Reader update

--- src/main/java/coronavirusTrack/Reader.java
+++ src/main/java/coronavirusTrack/Reader.java
@@ -4,19 +4,27 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.stream.Stream;
 
 public class Reader {
 
-	private ArrayBlockingQueue<String> readerqueue_;
+	private ArrayBlockingQueue<long[]> readerqueue_;
 	private String directory_;
 	private Date fin_date_;
 
 	// Constructor
 	
-	Reader(Date date, ArrayBlockingQueue<String> readerqueue, String directory ){
+	public Reader(Date date, ArrayBlockingQueue<long[]> readerqueue, String directory ){
 		
 		setReaderqueue_(readerqueue);
 		setDirectory_(directory);
@@ -26,16 +34,9 @@ public class Reader {
 	
 	// Functions
 	
-	public String Cut(String line) {
-		
-		String cut_line = null;
-		
-		
-		
-		return cut_line; 
-	}
 	
-	public void Search() {
+	
+	public void Search() throws ParseException, IOException {
 		// ----
 		//String path = p.getAbsolutePath()+"/csv/20";
 		// ----
@@ -45,37 +46,128 @@ public class Reader {
 		File[] csv;
 		p = new File(directory_);
 		csv = p.listFiles();
+		int [] i_line = new int[csv.length];
+		String [] csv_line = new String[csv.length];
+		int line_a_modif = 0;
+		java.util.Arrays.fill(i_line, 0);
+		int [] end_line = new int[csv.length];
+		for (int i=0; i < csv.length;i++)
+		{
+			end_line[i] = (int) Files.lines(csv[i].toPath()).count();
+		}
+		
 		
 		
 		// Loop
 		
-		int [] i_line = new int[csv.length];
-		java.util.Arrays.fill(i_line, 0);
+		
 		
 		for (int i=0; i < csv.length;i++)
 		{
 			//System.out.println(csv[i]);
+			for (int j=0; j < csv.length;j++) {
+				if (end_line[j] == i_line[j])
+				{
+					i_line[j] = -1;
+				}
+			}
+			
+			for (int j=0; j < csv.length;j++) {
+				if (i_line[j] == -1)
+				{
+					csv_line[j] = "none";
+				}
+				else
+				{
+					csv_line[j] = Read(i_line[j], csv[j].toString());
+				}	
+			}
+			
+			line_a_modif = Compare(csv_line, end_line, i_line);
+			if (line_a_modif == -1)
+			{
+				//envoie du colis piègé 
+			}
+			else
+			{
+				Cut(csv_line[line_a_modif]);
+			}
 			
 			
 			
 		}
 	}
 	
+	
 	public String Read(int n, String path )
 	{
 		 // The line number
 	      String line = null;
-	      try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-	          for (int i = 0; i < n; i++)
-	              br.readLine();
-	          line = br.readLine();
-	          //System.out.println(line);
-	      }
+	      try (Stream<String> lines = Files.lines(Paths.get(path))) {
+	          line = lines.skip(n).findFirst().get();
+	        }
 	      catch(IOException e){
 	        System.out.println(e);
 	      }
 		return line;
 	}
+	
+	@SuppressWarnings("null")
+	public int Compare(String t_line[], int end_line[], int i_line[]) throws ParseException
+	{
+		int modif = -1;
+		Date date[] = null;
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date mem_date = null;
+		
+		for (int i=0; i < t_line.length; i++)
+		{
+			if (i_line[i] != -1)
+			{
+				String[] split = t_line[i].split(",");
+				date[i] = format.parse(split[3]);
+				if (fin_date_.compareTo(date[i]) < 0)
+				{
+					if (mem_date == null)
+					{
+						mem_date = date[i];
+						modif = i;
+					}
+					else
+					{
+						if (mem_date.compareTo(date[i]) < 0)
+						{
+							mem_date = date[i];
+							modif = i;
+						}
+					}
+				}
+				else
+				{
+					i_line[i] = -1;
+				}
+			}
+		}
+		
+		
+		return modif;
+	}
+	
+	public void Cut(String line) {
+		
+
+		String[] split = line.split(", ");
+		
+		String cut_line = split[0]+","+split[3]+","+split[4];
+		
+		 
+	}
+	
+	public void PutIntoQ(long chaine[]) 
+	{
+		
+	}
+	
 	
 	
 	
@@ -84,12 +176,12 @@ public class Reader {
 	
 	// Get & Set
 	
-	public ArrayBlockingQueue<String> getReaderqueue_() {
+	public ArrayBlockingQueue<long[]> getReaderqueue_() {
 		return readerqueue_;
 	}
 
-	public void setReaderqueue_(ArrayBlockingQueue<String> readerqueue_) {
-		this.readerqueue_ = readerqueue_;
+	public void setReaderqueue_(ArrayBlockingQueue<long[]> readerqueue) {
+		this.readerqueue_ = readerqueue;
 	}
 
 	public String getDirectory_() {
@@ -108,8 +200,8 @@ public class Reader {
 
 
 
-	public void setFin_date_(Date fin_date_) {
-		this.fin_date_ = fin_date_;
+	public void setFin_date_(Date calendar) {
+		this.fin_date_ = calendar;
 	}
 	
 }
--- src/test/java/coronavirusTest/ReaderTest.java
+++ src/test/java/coronavirusTest/ReaderTest.java
@@ -1,5 +1,38 @@
 package coronavirusTest;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+//import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import org.junit.Test;
+
+import coronavirusTrack.Reader;
+
 public class ReaderTest {
 
+	@Test
+	public void test() throws ParseException {
+		File p = new File(".");
+		String path = p.getAbsolutePath()+"/csv/20/France.csv";
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date date = format.parse("2020-01-30 00:00:00");
+		ArrayBlockingQueue<long[]> readerqueue_=new ArrayBlockingQueue<long[]>(20); // non utilisé dans un premier temps
+
+		Reader monR =new Reader(date,readerqueue_,path);
+		
+		String test1 = monR.Read(3, path);
+		System.out.println(test1);
+		String test2 = monR.Cut(test1);
+		System.out.println(test2);
+		//assertEquals("13,1976-03-12 00:00:00,1587417223.6139328", test2);
+		
+		
+	 
+	}
 }
