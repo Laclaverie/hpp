4b01b9e65d4ae33a83a0d2ef6b9af8aab35674ca
julienchavas <julien.chavas1999@gmail.com>
2021-06-02 00:50:18 +0200

    save pour changement Reader

--- src/test/java/coronavirusTest/ReaderTest.java
+++ src/test/java/coronavirusTest/ReaderTest.java
@@ -36,10 +36,11 @@ public class ReaderTest {
 		//assertEquals("13,1976-03-12 00:00:00,1587417223.6139328", test2);
 		
 		System.out.println("Test Ultime");
+		System.out.println("Test en cour ...");
 		
 		monR.Search();
 		
-		
+		System.out.println("Test Fini !");
 		
 	 
 	}
