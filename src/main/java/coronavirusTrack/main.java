f55f5a7aa8a4f74faadd6b5c264c4d0ae0453cf8
laclaverie.pierre <pierre.laclaverie@telecom-st-etienne.fr>
2021-06-02 01:20:36 +0200

    wittter queue a avancer

--- src/main/java/coronavirusTrack/main.java
+++ src/main/java/coronavirusTrack/main.java
@@ -1,10 +1,34 @@
 package coronavirusTrack;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int numberofThread = 4;
+		ExecutorService service = Executors.newFixedThreadPool(numberofThread);
+		
+		shutdownAndAwaitTermination(service);
 
 	}
-
+	static void shutdownAndAwaitTermination(ExecutorService pool) {
+		pool.shutdown(); // Disable new tasks from being submitted
+		try {
+			// Wait a while for existing tasks to terminate
+			if (!pool.awaitTermination(30, TimeUnit.SECONDS)) {
+				pool.shutdownNow(); // Cancel currently executing tasks
+				// Wait a while for tasks to respond to being cancelled
+				if (!pool.awaitTermination(60, TimeUnit.SECONDS))
+					System.err.println("Pool did not terminate");
+			}
+		} catch (InterruptedException ie) {
+			// (Re-)Cancel if current thread also interrupted
+			pool.shutdownNow();
+			// Preserve interrupt status
+			Thread.currentThread().interrupt();
+		}
+	}
 }
